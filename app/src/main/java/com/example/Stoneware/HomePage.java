4be41e275ebaa9cd5d3b25b8143269e990c9bc23
azrudeen <89297978+azrudeen@users.noreply.github.com>
2024-03-30 23:18:05 +0530

    verion 1.1.4 Navigation drawer added and intigrated with HomePage.java works fine sucessfully .

--- app/src/main/java/com/example/Stoneware/HomePage.java
+++ app/src/main/java/com/example/Stoneware/HomePage.java
@@ -1,15 +1,34 @@
 package com.example.Stoneware;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class HomePage extends AppCompatActivity {
+
+
+    DrawerLayout drawerLayout;
+    ImageButton toolbar_menu_icon;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home_page);
+        setContentView(R.layout.navigation_drawer);
+
+        drawerLayout = findViewById(R.id.drawerLayout);
+        toolbar_menu_icon = findViewById(R.id.toolbar_menu_icon);
+
+        toolbar_menu_icon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawerLayout.open();
+            }
+        });
+
     }
 }
 
